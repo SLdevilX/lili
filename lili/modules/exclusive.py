e619d660c96304c7f0395ab3f992ba9396a3ad22
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-04-12 11:36:38 +0530

    Add files via upload

---
+++ lili/modules/exclusive.py
@@ -0,0 +1,24 @@
+__help__ = """
+*✖ URL Upload ✖*
+ ⦁ `/up`*:* reply to a direct download link to upload it to telegram as files.
+
+*✖ File To Link ✖*
+ ⦁ `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link.
+ ⦁ `/tmpninja`*:* reply to a telegram file to upload it on tmpninja and get direct download link.
+ ⦁ `/getlink`*:* reply to telegram file to get direct download link quickly.
+
+*✖ Screenshot Gen ✖*
+ ⦁ `/genss`*:* reply to a video to get some screen shots of it.
+
+*✖ File Renamer ✖*
+ ⦁ `/rename newfilename.extension`*:* rename any telegram media file.
+
+*✖ Convert To Video ✖*
+ ⦁ `/c2v`*:* reply to video file to convert it as streamable video.
+
+*✖ Virus Scan ✖*
+ ⦁ `/scanit`*:* Scan a file for virus (MAX SIZE = 3MB)
+
+*~ @TheliliBot 👸*"""
+
+__mod_name__ = "🧨 Exclusive 🧩"
